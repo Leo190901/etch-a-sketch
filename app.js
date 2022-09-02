1b8aed676b3acab8a549a3ad62871ac5053a04ce
Leon Prenkocevic <leonprenkocevic40@gmail.com>
2022-09-02 12:16:27 +0200

    created grid

--- app.js
+++ app.js
@@ -7,18 +7,21 @@ for(let i = 0; i < 4; i++){
     let div = document.createElement('div');
     div.classList.add('box');
     div.id = i;
-    div.innerText = 'asfsd';
     container.appendChild(div);
 
 }
 
 for(let i = 0; i < 4; i++){
-    let div2 = document.createElement('div');
-    div2.classList.add('box');
-    div2.innerText = 'asfsd';
-    let element = document.getElementById(i);
-    element.appendChild(div);
+    for(j = 0; j < 4; j++){
+        let div = document.createElement('div');
+        div.classList.add('sub-box');
+        
+        box = document.getElementById(i)
+        box.appendChild(div);
+    }
 }
 
+document.getElementsByClassName('sub-box').onmouseover = function (evt) {
+    this.parentNode.style.backgroundColor = "red";
 
-
+}
